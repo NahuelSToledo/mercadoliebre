26a65519bbbedcaae04a8010235c732d49b8ed6d
nahuelstoledo <nahueltoledo5@gmail.com>
2021-12-22 01:39:49 -0300

    mercadoliebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { get } = require("http");
+const path = require('path');
+
+const app = express();
+
+
+const staticFiles = express.static('public');
+app.use(staticFiles)
+
+
+app.listen(3500, () => console.log('Tenemos la Web'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+});
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});

86d79437ff56688acbeaf180b0e159a909ea0aed
nahuelstoledo <nahueltoledo5@gmail.com>
2021-12-22 02:00:44 -0300

    agregue process.env.port

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const staticFiles = express.static('public');
 app.use(staticFiles)
 
 
-app.listen(3500, () => console.log('Tenemos la Web'));
+app.listen(process.env.PORT || 3000, () => console.log('Tenemos la Web'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
